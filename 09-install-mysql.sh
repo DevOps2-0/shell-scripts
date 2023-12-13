e8b87a73004b7f7fca7204533d3e20d2b1db67e3
NagaYara <yrnp2355@gmail.com>
2023-12-13 14:31:09 +0530

    shell script labs

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -15,16 +15,16 @@ yum install mysql -y
 
 if [ $? -ne 0 ]
 then
-    echo "Error:: MySQL installation failed"
+    echo "Error:: MySQL installation is failed"
 else:
-    echo "Success:: MySQl Installation Success"
+    echo "Success:: MySQl Installation is Success"
 fi
 
 yum install git -y
 
 if [ $? -ne 0 ]
 then
-    echo "Error:: GIT Installation failed"
+    echo "Error:: GIT Installation is failed"
 else
-    echo "Success:: GIT Installation success"
+    echo "Success:: GIT Installation is success"
 fi
