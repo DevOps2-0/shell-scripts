9e109c3b2fb0f6491ca872a7d626249541cecfa9
NagaYara <yrnp2355@gmail.com>
2023-12-13 14:43:41 +0530

    shell script labs

--- 10-functions.sh
+++ 10-functions.sh
@@ -2,10 +2,25 @@
 
 ID=$(id -u)
 
-
 if [ $ID -ne 0 ]
 then
     echo "Error:: Please run this script with root access"
     exit 1
 fi
+
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]
+then
+    echo "Error:: $2... is failed"
+else:
+    echo "Success:: $2... is Success"
+fi
+
+}
+
+yum install mysql -y
+
+VALIDATE $? "Installing MySQL"
 
