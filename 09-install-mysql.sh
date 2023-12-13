7579de40e4dae28e10c698329dcc4d073221b8c9
NagaYara <yrnp2355@gmail.com>
2023-12-13 14:28:29 +0530

    shell script labs

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
 ID=$(id -u)
-DATE=$($0)
 
-echo "Script Name: $DATE"
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR:: Please run this script with root access"
+    echo "Error:: Please run this script with root access"
     exit 1
 else
     echo "You are the root user"
 fi
 
-# yum install mysql -y
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Error:: MySQL installation failed"
+else:
+    echo "Success:: MySQl Installation Success"
+fi
+
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "Error:: GIT Installation failed"
+else
+    echo "Success:: GIT Installation success"
+fi
